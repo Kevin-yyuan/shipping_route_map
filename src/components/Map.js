af2b7969302b1d7e35edfa6f706f988b5da38de4
Kevin.yy <99286497+Kevin-yyuan@users.noreply.github.com>
2025-09-10 16:04:39 -0400

    Adding Colors to the line

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MapContainer, TileLayer, Polyline, CircleMarker, Popup, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { getCurvedPath } from '../utils/curves';
+import { getColorForCustomer } from '../utils/color';
 
 const Map = ({ filteredTrips }) => {
   const position = [45.5017, -93.5673]; // Centered for the new routes
@@ -11,14 +12,15 @@ const Map = ({ filteredTrips }) => {
   const endPointCounts = {};
 
   filteredTrips.forEach(trip => {
-    const routeKey = `${trip.startPoint.name}-${trip.endPoint.name}`;
+    const routeKey = `${trip.startPoint.name}-${trip.endPoint.name}-${trip.customer}`;
     if (!routeInfo[routeKey]) {
       routeInfo[routeKey] = {
         count: 0,
         totalTime: 0,
         distance: trip.distance,
         startPoint: trip.startPoint,
-        endPoint: trip.endPoint
+        endPoint: trip.endPoint,
+        customer: trip.customer
       };
     }
     routeInfo[routeKey].count++;
@@ -57,11 +59,12 @@ const Map = ({ filteredTrips }) => {
         const curvedPath = getCurvedPath(route.startPoint.coords, route.endPoint.coords);
         return (
           <Polyline
-            key={`${route.startPoint.name}-${route.endPoint.name}`}
+            key={`${route.startPoint.name}-${route.endPoint.name}-${route.customer}`}
             positions={curvedPath}
-            pathOptions={{ color: 'blue', weight: route.count * 2 }}
+            pathOptions={{ color: getColorForCustomer(route.customer), weight: route.count * 2 }}
           >
             <Tooltip>
+              <strong>Customer:</strong> {route.customer}<br />
               <strong>From:</strong> {route.startPoint.name}<br />
               <strong>To:</strong> {route.endPoint.name}<br />
               <strong>Trip Counts:</strong> {route.count}<br />
